22a4243eb543ebda479ff99de419ed41166f6afb
李卓 <falcomfans.lizhuo@163.com>
2018-05-14 18:12:42 +0800

    IM项目

--- bcp.EnterpriseApplicationCustomization/src/main/java/com/odib/bcp/eac/MainApplication.java
+++ bcp.EnterpriseApplicationCustomization/src/main/java/com/odib/bcp/eac/MainApplication.java
@@ -10,6 +10,8 @@ package com.odib.bcp.eac;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.MessageDigest;
+
 /**
  * <br>
  * 〈主函数〉
@@ -19,7 +21,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class MainApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+    public static void main(String[] args) throws Exception{
+        //SpringApplication.run(MainApplication.class, args);
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        byte[] digest = md5.digest("lang=zh&location=116.40,39.9&t=1526286792&unit=m&username=HE18050813365716470850d8f6b3f942008cd753614a8d4f34".getBytes("UTF-8"));
+        Integer.parseInt("", 16);
+        String value = "4f32b09055fc0aa8b458cff4c58ace95";
+        byte[] bytes = toBytes(value);
+        System.out.println(bytes);
+    }
+    public static byte[] toBytes(String str) {
+        if(str == null || str.trim().equals("")) {
+            return new byte[0];
+        }
+
+        byte[] bytes = new byte[str.length() / 2];
+        for(int i = 0; i < str.length() / 2; i++) {
+            String subStr = str.substring(i * 2, i * 2 + 2);
+            bytes[i] = (byte) Integer.parseInt(subStr, 16);
+        }
+
+        return bytes;
     }
 }
